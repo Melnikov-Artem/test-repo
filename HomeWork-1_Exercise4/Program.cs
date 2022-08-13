3d4e2e75b9eb4ca8f8048164ca117794269d11b1
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-13 22:11:12 +0300

    HomeWork 1 Exercise 4

---
+++ HomeWork-1_Exercise4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 8:  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int index = 1;
+string count = "";
+while (N>(index-1))
+{
+    if (index % 2 == 0)
+        {
+            Console.Write($"{index}, ");
+            //count = count + ($"{index}, ");
+        }
+    index++;
+}
+//Console.WriteLine(count);
+Console.WriteLine();
