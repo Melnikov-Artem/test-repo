6a236dc5129037a6261e5bc5edae451cda676161
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-15 19:09:43 +0300

    HomeWork 3 Exercise 3

---
+++ HomeWork-3_Exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Clear();
+Console.Write("Введите число, для которого будет выдаваться список возведения в 3-ю степень:");
+int number = Convert.ToInt32(Console.ReadLine());
+int i;
+
+for (i = 1; i < number;)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+    ++i;
+}
+Console.WriteLine(Math.Pow(i,3));
